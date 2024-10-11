0be3cbadb50f1f6897b69c8d6d6320d2e4fe0d44
caikeferreira <caikeferreiradev@gmail.com>
2024-10-11 16:40:32 -0300

    OrderItem Model

---
+++ lib/models/orderitem.dart
@@ -0,0 +1,33 @@
+import 'package:front_end/models/product.dart';
+
+class OrderItem {
+  num amount;
+  num value;
+  Product? product;
+
+  OrderItem({
+    required this.amount,
+    required this.value,
+    required this.product,
+  });
+
+  OrderItem.formJson(Map<String, dynamic> json)
+      : amount = json['amount'],
+        value = json['value'],
+        product =
+            json['product'] != null ? Product.formJson(json['product']) : null;
+
+  static List<OrderItem> formJsonList(Map<String, dynamic> json) {
+    List<dynamic>? lists = (json['items'] as List<dynamic>?);
+
+    if (lists == null) return [];
+
+    return lists.map((itemJson) => OrderItem.formJson(itemJson)).toList();
+  }
+
+  Map<String, dynamic> toJson() => {
+        'amount': amount,
+        'value': value,
+        'productId': product!.id,
+      };
+}
