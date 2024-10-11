bf0f7692848a2e5118c22d8a01713a94de9bae14
caikeferreira <caikeferreiradev@gmail.com>
2024-10-11 16:40:03 -0300

    Order Model

---
+++ lib/models/order.dart
@@ -0,0 +1,28 @@
+import 'package:front_end/models/orderitem.dart';
+
+class Order {
+  num id;
+  num clientId;
+  DateTime date;
+  List<OrderItem> orderItems;
+
+  Order({
+    this.id = 0,
+    required this.date,
+    required this.clientId,
+    required this.orderItems,
+  });
+
+  Order.formJson(Map<String, dynamic> json)
+      : id = json['id'],
+        clientId = json['clientId'],
+        date = DateTime.parse(json['date']),
+        orderItems = OrderItem.formJsonList(json);
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'clientId': clientId,
+        'date': date.toIso8601String(),
+        'items': orderItems.map((orderItem) => orderItem.toJson()).toList(),
+      };
+}
