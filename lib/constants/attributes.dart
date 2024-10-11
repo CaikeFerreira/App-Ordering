34bbd0bf9b65a7fd17f42baf28495602f53cc0f4
caikeferreira <caikeferreiradev@gmail.com>
2024-10-11 16:46:23 -0300

    Create Attributes

---
+++ lib/constants/attributes.dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+const defaultPadding = 8.0;
+const double defaultRadiusBorder = 5;
+
+const locale = "pt-BR";
+
+const fontSize = 12;
+
+const dataFormat = "dd/MM/yyyy HH:mm";
+
+NumberFormat formatMoeda = NumberFormat.simpleCurrency(locale: locale);
