4a9e75573ff0ec737fa9ee7f03736a31c8425472
caikeferreira <caikeferreiradev@gmail.com>
2024-10-11 16:47:46 -0300

    Create LoginHttp

---
+++ lib/http/logins/login_http.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:front_end/constants/uris.dart';
+import 'package:front_end/models/login.dart';
+import 'package:http/http.dart';
+
+import '../httpclient.dart';
+
+class LoginHttp {
+  Future<Login> find(Login login) async {
+    final Response response = await client.post(
+      loginUri(),
+      headers: headers,
+      body: jsonEncode(login.toJson()),
+    );
+
+    if (response.statusCode >= 200 && response.statusCode <= 299) {
+      return Login.formJson(jsonDecode(response.body));
+    }
+
+    if (response.statusCode == 404) {
+      return login;
+    }
+
+    throw HttpException(getMessage(response.statusCode));
+  }
+}
