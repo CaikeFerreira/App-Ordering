08574a6a4b97d5f86c3dd899cba23a000e9e5c1f
caikeferreira <caikeferreiradev@gmail.com>
2024-10-14 12:59:00 -0300

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:front_end/screens/home/home_screen.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 
 void main() {
-  runApp(const MainApp());
+  //Barrando a orientação em retrato
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      title: 'App Ordering',
+      debugShowCheckedModeBanner: false,
+      theme: FlexThemeData.light(
+        scheme: FlexScheme.blue,
+        subThemesData: const FlexSubThemesData(
+          interactionEffects: false,
+          tintedDisabledControls: false,
+          blendOnColors: false,
+          useTextTheme: true,
+          inputDecoratorUnfocusedHasBorder: false,
+          inputDecoratorFocusedHasBorder: false,
+          //alignedDropdown: true,
+          tooltipRadius: 4,
+          tooltipSchemeColor: SchemeColor.inverseSurface,
+          tooltipOpacity: 0.9,
+          useInputDecoratorThemeInDialogs: true,
+          snackBarElevation: 6,
+          snackBarBackgroundSchemeColor: SchemeColor.inverseSurface,
+          appBarScrolledUnderElevation: 20.0,
+          navigationBarSelectedLabelSchemeColor: SchemeColor.onSurface,
+          navigationBarUnselectedLabelSchemeColor: SchemeColor.onSurface,
+          navigationBarMutedUnselectedLabel: false,
+          navigationBarSelectedIconSchemeColor: SchemeColor.onSurface,
+          navigationBarUnselectedIconSchemeColor: SchemeColor.onSurface,
+          navigationBarMutedUnselectedIcon: false,
+          navigationBarIndicatorSchemeColor: SchemeColor.secondaryContainer,
+          navigationBarIndicatorOpacity: 1.00,
+          navigationRailSelectedLabelSchemeColor: SchemeColor.onSurface,
+          navigationRailUnselectedLabelSchemeColor: SchemeColor.onSurface,
+          navigationRailMutedUnselectedLabel: false,
+          navigationRailSelectedIconSchemeColor: SchemeColor.onSurface,
+          navigationRailUnselectedIconSchemeColor: SchemeColor.onSurface,
+          navigationRailMutedUnselectedIcon: false,
+          navigationRailIndicatorSchemeColor: SchemeColor.secondaryContainer,
+          navigationRailIndicatorOpacity: 1.00,
+          navigationRailBackgroundSchemeColor: SchemeColor.surface,
+          navigationRailLabelType: NavigationRailLabelType.none,
         ),
+        keyColors: const FlexKeyColors(),
+        visualDensity: FlexColorScheme.comfortablePlatformDensity,
+        useMaterial3: true,
+        swapLegacyOnMaterial3: true,
+        // To use the Playground font, add GoogleFonts package and uncomment
+        fontFamily: GoogleFonts.notoSans().fontFamily,
       ),
+      home: const HomeContainer(),
     );
   }
 }
+
