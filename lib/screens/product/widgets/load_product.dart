6da44f1d3a32459bd3e75d820f3915b21d1b0c3a
caikeferreira <caikeferreiradev@gmail.com>
2024-10-17 13:22:09 -0300

    Refactoring Load Product

--- lib/screens/product/widgets/load_product.dart
+++ lib/screens/product/widgets/load_product.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:front_end/screens/cart/cart_screen.dart';
 
+import '../../cart/cart_screen.dart';
 import '../../../models/product.dart';
 import '../../../screen_widgets/cardproduct/cardproduct.dart';
 
-Widget LoadProduct(BuildContext context, List<Product> products) {
+Widget loadProduct(BuildContext context, List<Product> products) {
   products.sort((a, b) => a.name.compareTo(b.name));
   return ListView(
     children: [
-      SizedBox(height: 50),
+      const SizedBox(height: 70),
       for (Product product in products)
         CardProduct.addCart(
           product: product,
