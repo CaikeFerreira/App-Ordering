e8448cd38c877798e70c0e575dce7d94c700edd2
caikeferreira <caikeferreiradev@gmail.com>
2024-10-17 13:19:27 -0300

    Create ImagePath

---
+++ lib/constants/image_path.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Imagez {
+  static final logo = Image.asset("assets/images/logo256x256.png");
+
+  static const loginTop = "assets/images/top1.png";
+  static const loginTop2 = "assets/images/top2.png";
+  static const loginBottom = "assets/images/bottom1.png";
+  static const loginBottom2 ="assets/images/bottom2.png";
+}
